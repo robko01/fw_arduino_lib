847ed4c30546ec175747f93374f2b7e46b125cdd
Orlin Dimitrov <orlin369@gmail.com>
2021-11-09 22:27:26 +0200

    Generate helper

--- src/GeneralHelper.cpp
+++ src/GeneralHelper.cpp
@@ -37,3 +37,148 @@ uint8_t bit_array_to_uint8(bool arr[], uint8_t count)
 
 	return ResultL;
 }
+
+/** @brief Get MAC address.
+ *  @return String, Returns the string of MAC address.
+ */
+String mac2str(const uint8_t * mac) {
+
+	char MACStrL[WL_MAC_ADDR_LENGTH * 3] = { 0 };
+
+	sprintf(MACStrL, "%02X:%02X:%02X:%02X:%02X:%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+
+	return  String(MACStrL);
+}
+
+/** @brief Convert a single hex digit character to its integer value. Based on https://code.google.com/p/avr-netino/
+ *  @return String, Returns the string of unified URL string.
+ */
+unsigned char hex2dec(char c) {
+
+	if (c >= '0' && c <= '9') {
+		return((unsigned char)c - '0');
+	}
+	if (c >= 'a' && c <= 'f') {
+		return((unsigned char)c - 'a' + 10);
+	}
+	if (c >= 'A' && c <= 'F') {
+		return((unsigned char)c - 'A' + 10);
+	}
+	return(0);
+}
+
+/** @brief Converts binary array to heximal string.
+ *  @param uint8_t * input, Binary input.
+ *  @param unsigned int input_size, Binary input size.
+ *  @param char * output, Output result.
+ *  @return boolean, Returns the true if value is in the range.
+ */
+void bin_to_strhex(uint8_t *input, unsigned int input_size, uint8_t *output)
+{
+	for (unsigned int index = 0; index < input_size; index++)
+	{
+		output[index * 2 + 0] = hex2dec((input[index] >> 4) & 0x0F);
+		output[index * 2 + 1] = hex2dec((input[index]) & 0x0F);
+	}
+}
+
+/** @brief Check the Values is between: [0 - 255].
+ *  @param value String, Value of the octet.
+ *  @return boolean, Returns the true if value is in the range.
+ */
+boolean check_octet_range(String value) {
+	return check_octet_range(value.toInt());
+}
+
+/** @brief Check the Values is between: [0 - 255].
+ *  @param value int, Value of the octet.
+ *  @return boolean, Returns the true if value is in the range.
+ */
+boolean check_octet_range(int value) {
+	return (value > 0 && value < 256);
+}
+
+/** @brief Convert size to nice text type.
+ *  @param bytes, size_t Size of the file.
+ *  @return String, Nice formated size text.
+ */
+String formatBytes(size_t bytes) {
+	if (bytes < 1024UL) {
+		return String(bytes) + "B";
+	}
+	else if (bytes < (1048576UL)) {
+		return String(bytes / 1024UL) + "KB";
+	}
+	else if (bytes < (1073741824UL)) {
+		return String(bytes / 1048576UL) + "MB";
+	}
+	else {
+		return String(bytes / 1073741824UL) + "GB";
+	}
+}
+
+/** @brief Convert many minutes to ending minutes.
+ *  @param int minutes, Minutest.
+ *  @return int, Ending minutes.
+ */
+int to_minutes(int minutes)
+{
+	return minutes % 60;
+}
+
+/** @brief Convert many minutes to hours.
+ *  @param int minutes, Minutest.
+ *  @return int, Hours.
+ */
+int to_hours(int minutes)
+{
+	return minutes / 60;
+}
+
+/** @brief Printout in the debug console flash state.
+ *  @return Void.
+ */
+void show_device_properties() {
+#ifdef SHOW_FUNC_NAMES
+	DEBUGLOG("\r\n");
+	DEBUGLOG(__PRETTY_FUNCTION__);
+	DEBUGLOG("\r\n");
+#endif // SHOW_FUNC_NAMES
+
+#if defined(ESP8266) || defined(ESP32)
+
+#if defined(ESP8266)
+// ESP8266
+	DEBUGLOG("Flash chip size: %u\r\n", ESP.getFlashChipRealSize());
+#endif
+
+	DEBUGLOG("Sketch size: %u\r\n", ESP.getSketchSize());
+	DEBUGLOG("Free flash space: %u\r\n", ESP.getFreeSketchSpace());
+	DEBUGLOG("Free heap: %d\r\n", ESP.getFreeHeap());
+	//DEBUGLOG("Firmware version: %d\r\n", ESP_FW_VERSION);
+	DEBUGLOG("SDK version: %s\r\n", ESP.getSdkVersion());
+	DEBUGLOG("MAC address: %s\r\n", WiFi.macAddress().c_str());
+	DEBUGLOG("\r\n");
+
+#endif
+}
+
+#ifdef ESP32
+
+#ifdef BATT_MONITOR
+
+/** @brief Read battery voltage.
+ *  @return float voltage.
+ */
+float battery_voltage(int pin)
+{
+	static float batt_voltage = 0.0;
+
+	float batt_voltage = (float)(analogRead(pin)/4095)*2*3.3*1.1;
+
+	return batt_voltage;
+}
+
+#endif
+
+#endif
--- src/GeneralHelper.h
+++ src/GeneralHelper.h
@@ -28,7 +28,97 @@
 	#include "WProgram.h"
 #endif
 
+#ifdef ESP32
+#include <WiFi.h>
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#include <ESP8266mDNS.h>
+#endif
+
+#include "DebugPort.h"
+
+#ifndef WL_MAC_ADDR_LENGTH
+#define WL_MAC_ADDR_LENGTH 6
+#endif
+
+#pragma region Prototypes
+
+/**
+ * @brief 
+ * 
+ * @param arr 
+ * @param count 
+ * @return uint8_t 
+ */
 uint8_t bit_array_to_uint8(bool arr[], uint8_t count);
+
+/**
+ * @brief Get MAC address.
+ * 
+ * @param mac 
+ * @return String, Returns the string of MAC address. 
+ */
+String mac2str(const uint8_t * mac);
+
+/** @brief Convert a single hex digit character to its integer value. Based on https://code.google.com/p/avr-netino/
+ *  @return String, Returns the string of unified URL string.
+ */
+unsigned char hex2dec(char c);
+
+/** @brief Converts binary array to heximal string.
+ *  @param unsigned char * input, Binary input.
+ *  @param unsigned int input_size, Binary input size.
+ *  @param char * output, Output result.
+ *  @return boolean, Returns the true if value is in the range.
+ */
+void bin_to_strhex(uint8_t *input, unsigned int input_size, uint8_t *output);
+
+/** @brief Check the Values is between: [0 - 255].
+ *  @param value String, Value of the octet.
+ *  @return boolean, Returns the true if value is in the range.
+ */
+boolean check_octet_range(String value);
+
+/** @brief Check the Values is between: [0 - 255].
+ *  @param value int, Value of the octet.
+ *  @return boolean, Returns the true if value is in the range.
+ */
+boolean check_octet_range(int value);
+
+/** @brief Convert size to nice text type.
+ *  @param bytes, size_t Size of the file.
+ *  @return String, Nice formated size text.
+ */
+String formatBytes(size_t bytes);
+
+/** @brief Convert many minutes to ending minutes.
+ *  @param int minutes, Minutest.
+ *  @return int, Ending minutes.
+ */
+int to_minutes(int minutes);
+
+/** @brief Convert many minutes to hours.
+ *  @param int minutes, Minutest.
+ *  @return int, Hours.
+ */
+int to_hours(int minutes);
+
+/** @brief Printout in the debug console flash state.
+ *  @return Void.
+ */
+void show_device_properties();
+
+#ifdef BATT_MONITOR
+
+/** @brief Read battery voltage.
+ *  @return float voltage.
+ */
+float battery_voltage(int pin);
+
+#endif
+
+#pragma endregion
 
 #endif
 
